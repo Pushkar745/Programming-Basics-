c705dd8c167cd1ed8d779dbbbf1b56f4163dcc38
Pushkar Sudhir Baviskar <b.pushkar745@gmail.com>
2020-12-19 20:04:22 +0530

    SumofDigit

---
+++ SumofDigit/SumofDigit/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumofDigit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, sum = 0, m;
+            Console.WriteLine("Enter the Number");
+            n = Convert.ToInt32(Console.ReadLine());
+            while(n>0)
+            {
+                m = n % 10;
+                sum = sum + m;
+                n = n / 10;
+            }
+            Console.WriteLine("Sum is =" + sum);
+            Console.ReadKey();
+        }
+    }
+}
