2358624e008bd33d4703da4ac22424d3227b57c0
Pushkar Sudhir Baviskar <b.pushkar745@gmail.com>
2020-12-19 19:31:25 +0530

    Palindrome Numbers

---
+++ PalindromeNumber/PalindromeNumber/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, r, sum = 0, temp;
+            Console.Write("Enter the no ");
+            n = Convert.ToInt32(Console.ReadLine());
+            temp = n;
+            while(n>0)
+            {
+                r = n % 10;
+                sum = (sum * 10) + r;
+                n = n / 10;
+
+            }
+            if (temp == sum)
+                Console.Write("Number is palindrome");
+            else
+                Console.Write("Number is not Palindrome");
+            Console.ReadKey();
+
+
+        }
+    }
+}
