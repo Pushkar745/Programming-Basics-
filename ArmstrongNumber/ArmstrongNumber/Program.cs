2408f8fda894429205688b3513ce97397aaf89a5
Pushkar Sudhir Baviskar <b.pushkar745@gmail.com>
2020-12-19 19:59:45 +0530

    Armstrong Number

---
+++ ArmstrongNumber/ArmstrongNumber/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArmstrongNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, r, sum = 0, temp;
+            Console.WriteLine("Enter The number =");
+            n = Convert.ToInt32(Console.ReadLine());
+            temp = n;
+            while(n>0)
+            {
+                r = n % 10;
+                sum = sum + (r * r * r);
+                n = n / 10;
+            }
+            if (temp == sum)
+                Console.Write("Armstrong number");
+            else
+                Console.Write("Not Armstrong Number");
+            Console.ReadKey();
+        }
+    }
+}
