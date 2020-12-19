92b23587527cd39328604aa65dda5817470cc032
Pushkar Sudhir Baviskar <b.pushkar745@gmail.com>
2020-12-19 22:40:39 +0530

    Swapping Of Two Numbers

---
+++ Swaping_Of_Two_Number/Swaping_Of_Two_Number/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swaping_Of_Two_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Enter The number a");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the number b");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Before Swap a="+a+ "b="+b);
+            a = a * b;
+            b = a / b;
+            a = a / b;
+            Console.WriteLine("After Swap a=" + a + "b=" + b );
+            Console.ReadKey();
+
+        }
+    }
+}
