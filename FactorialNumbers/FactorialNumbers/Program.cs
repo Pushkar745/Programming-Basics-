3057760a0206e820f47c30f9210b9d6738a4a776
Pushkar Sudhir Baviskar <b.pushkar745@gmail.com>
2020-12-19 19:51:30 +0530

    namespace FactorialNumbers

---
+++ FactorialNumbers/FactorialNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorialNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, fact = 1, number;
+            Console.WriteLine("Enter the any number");
+            number = Convert.ToInt32(Console.ReadLine());
+            for(i=1;i<number;i++)
+            {
+                fact = fact * i;
+
+            }
+            Console.Write("factorial" + number + "is" + fact);
+            Console.ReadKey();
+
+        }
+    }
+}
