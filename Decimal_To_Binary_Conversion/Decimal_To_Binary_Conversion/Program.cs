e2d3c0ab0ee9ac27c5ce3d8154513ab5de37bef2
Pushkar Sudhir Baviskar <b.pushkar745@gmail.com>
2020-12-19 22:49:38 +0530

    Decimal_To_Binary_Conversion

---
+++ Decimal_To_Binary_Conversion/Decimal_To_Binary_Conversion/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decimal_To_Binary_Conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, i;
+            int[] a = new int[10];
+            Console.Write("Enter the Number to Convert:");
+            n = Convert.ToInt32(Console.ReadLine());
+            for(i=0;n>0;i++)
+            {
+                a[i] = n % 2;
+                n = n / 2;
+            }
+            Console.Write("Binary Of the Given Number = ");
+            for(i=0;i>=0;i--)
+            {
+                Console.Write(a[i]);
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
