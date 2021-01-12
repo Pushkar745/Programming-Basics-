8d1cafe4112a0ed722dd4dca071e46701eefac72
Pushkar Sudhir Baviskar <b.pushkar745@gmail.com>
2021-01-12 18:35:27 +0530

    FibonacciTriangle

---
+++ FibonacciTriangle/FibonacciTriangle/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 1, i, c, n, j;
+            Console.WriteLine("Entetr the Range :");
+            n = Convert.ToInt32(Console.ReadLine());
+            for(i=1;i<=n;i++)
+            {
+                a = 0;
+                b = 1;
+                Console.Write(b + "\t");
+                for(j=1;j<i;j++)
+                {
+                    c = a + b;
+                    Console.Write(c + "\t");
+                    a = b;
+                    b = c;
+                }
+                Console.Write("\n");
+                Console.ReadLine();
+            }
+        }
+    }
+}
