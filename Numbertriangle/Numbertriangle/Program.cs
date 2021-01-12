b824b09e5479b35145957b101201021a5101b198
Pushkar Sudhir Baviskar <b.pushkar745@gmail.com>
2021-01-12 18:27:34 +0530

    Number Triangle

---
+++ Numbertriangle/Numbertriangle/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numbertriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, k, l, n;
+            Console.Write("Enter The Range=");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= n; i++)
+            {
+                for (j = 1; j <= n - 1; j++)
+                {
+                    Console.Write("");
+
+                }
+                for (k = 1; k <= i; k++)
+                {
+                    Console.Write(k);
+                }
+                for (l = i - 1; l >= 1; l--)
+                {
+                    Console.Write(l);
+                }
+                Console.Write("\n");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
+    
