f8799e8139018f5f147c59dff1cf9de6a437c9e9
Pushkar Sudhir Baviskar <b.pushkar745@gmail.com>
2021-01-21 16:41:47 +0530

    Sample program for exp

---
+++ SampleProgram/SampleProgram/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace SampleProgram
+{
+    class Program
+    {
+        static void Mymethod()
+        {
+            Console.Write("Mymethod");
+
+        }
+        static void Name :Mymethod()
+            {
+            }
+        static void Main(string[] args)
+        {
+            Mymethod();
+            Console.ReadKey();
+
+        }
+        
+    }
+}
