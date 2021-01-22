ee9c11690611b0a5251832f827f4f5f7029ff464
Pushkar Sudhir Baviskar <b.pushkar745@gmail.com>
2021-01-22 11:49:03 +0530

    total number of people in room with algo

--- SampleProgram/SampleProgram/Program.cs
+++ SampleProgram/SampleProgram/Program.cs
@@ -5,18 +5,20 @@ namespace SampleProgram
 {
     class Program
     {
-        static void Mymethod()
-        {
-            Console.Write("Mymethod");
-
-        }
-        
             
         static void Main(string[] args)
         {
-            Mymethod();
-            Console.ReadKey();
+            int n,i;
+            Console.WriteLine("Enter the range");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (i = 0; i <= n; i++) Console.WriteLine("The range is " + i);
 
+            Console.ReadKey();
+            /* 
+             let N=0;
+             For Each pair of people in room
+             set n=n+1;
+             */
         }
         
     }
