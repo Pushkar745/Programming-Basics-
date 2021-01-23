fe5bf5f2c87565430ef5eb259200187e08522e21
Pushkar Sudhir Baviskar <b.pushkar745@gmail.com>
2021-01-23 16:58:31 +0530

    MethodOverloading

---
+++ MethodOverloading/MethodOverloading/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    class Program
+    {
+        static int Sum(int a,int b)
+        {
+            return a + b;
+
+        }
+        static double Sum(double a, double b)
+        {
+            return a * b;
+
+        }
+
+        static void Main(string[] args)
+        {
+            int Myno = Sum(5, 10);
+            double myNo = Sum(5.8,2.5);
+            Console.WriteLine(Myno);
+            Console.WriteLine(myNo);
+            Console.ReadLine();
+
+        }
+    }
+}
