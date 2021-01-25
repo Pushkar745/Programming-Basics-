6715790f3c86a3e8b270497b3286c6ec6b1cad96
Pushkar Sudhir Baviskar <b.pushkar745@gmail.com>
2021-01-25 16:47:08 +0530

    get set values

--- ParameteriesConstrutor/ParameteriesConstrutor/Program.cs
+++ ParameteriesConstrutor/ParameteriesConstrutor/Program.cs
@@ -9,18 +9,29 @@ namespace ParameteriesConstrutor
     class Program
     { 
 
-        public string model;
+       protected string model;
     public string color;
     public int year;
-    public Program(string modelName,string modeColor,int modelYear)
+        public Program(string modelName, string modeColor, int modelYear)
         {
             model = modelName;
             color = modeColor;
             year = modelYear;
+
         }
-        static void Main(string[] args)
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+            static void Main(string[] args)
         {
             Program p = new Program("Mustang","red",1995);
+            p.Name = "pushkar";
+            Console.WriteLine(p.name);
+
             Console.WriteLine(p.color + "" + p.year + "" + p.model);
             Console.ReadLine();
         }
