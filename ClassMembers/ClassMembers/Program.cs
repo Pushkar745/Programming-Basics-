2ace5f7dcd7df5bbb96c32b0309df6daad27f4ab
Pushkar Sudhir Baviskar <b.pushkar745@gmail.com>
2021-01-25 13:21:19 +0530

    New program Constructor and some changes in class members

--- ClassMembers/ClassMembers/Program.cs
+++ ClassMembers/ClassMembers/Program.cs
@@ -8,21 +8,7 @@ namespace ClassMembers
 {
     class Program
     {
-        class MyClass
-
-        {
-
-            //Fields and methods inside classes are often referred to as "Class Members":
-            string Name = "Pushkar"; //field
-            int Salary = 5000000;//field
-            int Age=27;
-            public void Job(string age="27") //Method
-            {
-                Console.WriteLine("My name is :" + Name);
-                Console.WriteLine("My Salary:"+Salary);
-                Console.WriteLine(age);
-            }
-        }
+        
        
         static void Main(string[] args)
         {
