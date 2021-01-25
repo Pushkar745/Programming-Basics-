78448b96a620e2b90e0071e62550a01f3c2fc7df
Pushkar Sudhir Baviskar <b.pushkar745@gmail.com>
2021-01-25 12:56:13 +0530

    Fields and Methods

---
+++ ClassMembers/ClassMembers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMembers
+{
+    class Program
+    {
+        class MyClass
+
+        {
+
+            //Fields and methods inside classes are often referred to as "Class Members":
+            string Name = "Pushkar"; //field
+            int Salary = 5000000;//field
+            int Age=27;
+            public void Job(string age="27") //Method
+            {
+                Console.WriteLine("My name is :" + Name);
+                Console.WriteLine("My Salary:"+Salary);
+                Console.WriteLine(age);
+            }
+        }
+       
+        static void Main(string[] args)
+        {
+            MyClass p = new MyClass();
+            p.Job();
+           
+            Console.ReadLine();
+
+        }
+    }
+}
