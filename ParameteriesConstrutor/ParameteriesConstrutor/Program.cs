3df8051cc76dd2760cad3fd28deb192f6d43b0d2
Pushkar Sudhir Baviskar <b.pushkar745@gmail.com>
2021-01-25 16:34:52 +0530

    namespace ParameteriesConstrutor

---
+++ ParameteriesConstrutor/ParameteriesConstrutor/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParameteriesConstrutor
+{
+    class Program
+    { 
+
+        public string model;
+    public string color;
+    public int year;
+    public Program(string modelName,string modeColor,int modelYear)
+        {
+            model = modelName;
+            color = modeColor;
+            year = modelYear;
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program("Mustang","red",1995);
+            Console.WriteLine(p.color + "" + p.year + "" + p.model);
+            Console.ReadLine();
+        }
+    }
+}
