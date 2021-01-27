ac6166114623844a174cb3da69aeb35eec4fca5e
Pushkar Sudhir Baviskar <b.pushkar745@gmail.com>
2021-01-27 20:26:36 +0530

    Example of inheritance

---
+++ Inheritance1/Inheritance1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance1
+{
+    class Vehical //base class(Parent class)
+    {
+        public string brand = "Ford";
+        public void honk()
+        {
+            Console.WriteLine("Tuut,Tuut!");
+        }
+    }
+    class car:Vehical//derived class(Child)
+    {
+        public string modelName = "Mustang";
+
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            car c = new car();
+            c.honk();
+            Console.WriteLine(c.brand+""+c.modelName);
+            Console.ReadKey();
+        }
+    }
+}
