ef4e83cc019ff249b796227ae594420bc7b30787
Pushkar Sudhir Baviskar <b.pushkar745@gmail.com>
2021-01-27 20:44:39 +0530

    simple example Polymorphism

---
+++ Polymorphism0/Polymorphism0/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism0
+{
+    class Animal
+    {
+        public void animalSound()
+        {
+            Console.WriteLine("Animal Sound");
+        }
+    }
+    class Dog :Animal
+    {
+        public void dogsound()
+        {
+            Console.WriteLine("Bow bOw");
+        }
+    }
+    class Cat:Animal
+    {
+        public void catSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal a = new Animal();
+            Dog d = new Dog();
+            Cat c = new Cat();
+            a.animalSound();
+            Console.WriteLine();
+            d.dogsound();
+            c.catSound();
+            Console.ReadLine();
+
+        }
+    }
+}
