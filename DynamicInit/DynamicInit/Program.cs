47bacce0ab54fe25e2be1483e6340c456cfc1d87
Pushkar Sudhir Baviskar <b.pushkar745@gmail.com>
2021-01-28 15:32:56 +0530

    Demostrate dynamic initialization

---
+++ DynamicInit/DynamicInit/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicInit
+{
+    class Program
+    {//demostrate dynamic initialization
+        static void Main(string[] args)
+        {
+            //Length of sides
+            double s1 = 4.0;
+            double s2 = 5.0;
+            //Dynamically init hypot
+            double hypot = Math.Sqrt((s1 * s1) + (s2 * s2));
+            Console.WriteLine("Hypotenus of triange with sides"+s1+"by"+s2+ "is");
+            Console.WriteLine("{0:#.###}.", hypot);
+            Console.ReadLine();
+
+                 
+        }
+    }
+}
