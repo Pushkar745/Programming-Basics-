bdad03b4ba2894f9bfb95a19c51dd7987e3a4492
Pushkar Sudhir Baviskar <b.pushkar745@gmail.com>
2021-01-28 11:26:11 +0530

    Inside Class and Assign enum values

--- Enum0/Enum0/Program.cs
+++ Enum0/Enum0/Program.cs
@@ -6,17 +6,24 @@ using System.Threading.Tasks;
 
 namespace Enum0
 {
+    //Enum is short for "enumerations", which means "specifically listed"
     class Program
     {
         enum Friends{
-            Pushkar,
-           Manasi,
-
+            Pushkar,//Enum inside a Class
+            Manasi,
+           ABC=8,//You can also assign your own enum values,
+            XYZ
         }
         static void Main(string[] args)
         {
             Friends f = Friends.Manasi;
+            int f1 = (int)Friends.Manasi;//Enum Values
+            int f2 = (int)Friends.ABC;
+
             Console.WriteLine(f);
+            Console.WriteLine(f1);
+            Console.WriteLine(f2);
             Console.ReadLine();
         }
     }
