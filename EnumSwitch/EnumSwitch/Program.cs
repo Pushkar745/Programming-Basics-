17a53e793f90240056d73606d57eb1bdeedea662
Pushkar Sudhir Baviskar <b.pushkar745@gmail.com>
2021-01-28 11:34:17 +0530

    Enum Using Switch

---
+++ EnumSwitch/EnumSwitch/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumSwitch
+{
+    class Program
+    {
+        enum Level
+        {
+            Low,
+            Medium,
+            High
+        }
+        static void Main(string[] args)
+        {
+            Level l = Level.High;
+            switch(l)
+            {
+                case Level.Low:
+                    Console.WriteLine("Low Level");
+                    break;
+                case Level.Medium:
+                    Console.WriteLine("Medium Level");
+                    break;
+                case Level.High:
+                    Console.WriteLine("High level");
+                    break;
+                   
+            }
+            Console.ReadLine();
+        }
+    }
+}
