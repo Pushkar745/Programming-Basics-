9b0dafac318f895c0f2380097b347882515a969c
Pushkar Sudhir Baviskar <b.pushkar745@gmail.com>
2021-01-28 11:54:01 +0530

    File Handling With Exception

---
+++ Files0/Files0/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Files0
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            try
+            {
+                string MyText = "Hello World";
+                File.WriteAllText("M:/Programs/FileCSharp/Pushkar.txt", MyText);
+                string ReadText = File.ReadAllText(MyText);
+                Console.WriteLine(ReadText);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
+         
+        }
+        }
+    }
+
