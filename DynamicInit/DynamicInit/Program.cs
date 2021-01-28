2f38fa7d07dbeec5f5fe92180cf598df90803e97
Pushkar Sudhir Baviskar <b.pushkar745@gmail.com>
2021-01-28 16:05:26 +0530

    Demonstrate the relational and logical operators

--- DynamicInit/DynamicInit/Program.cs
+++ DynamicInit/DynamicInit/Program.cs
@@ -13,10 +13,17 @@ namespace DynamicInit
             //Length of sides
             double s1 = 4.0;
             double s2 = 5.0;
+            var s3 = 5;
+            var s4 = 6;
             //Dynamically init hypot
             double hypot = Math.Sqrt((s1 * s1) + (s2 * s2));
+            var hypot1 = Math.Sqrt((s3 * s3) + (s4 * s4));//These are now implicitly typed veriables.
+            //They are int type because their initializingexpressions are of type int
             Console.WriteLine("Hypotenus of triange with sides"+s1+"by"+s2+ "is");
             Console.WriteLine("{0:#.###}.", hypot);
+            Console.WriteLine("Hypotenus of triange with sides" + s3 + "by" + s4 + "is");
+            Console.WriteLine("{0:#.###}.", hypot1);
+            Console.WriteLine(typeof(int));
             Console.ReadLine();
 
                  
---
+++ RelLogOps/RelLogOps/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RelLogOps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            bool b1, b2;
+            i = 10;
+            j = 11;
+            if (i < j) Console.WriteLine("i<j");
+            if (i <= j) Console.WriteLine("i<=j");
+            if (i != j) Console.WriteLine("i!=j");
+            if (i == j) Console.WriteLine("This won't be executed");
+            if (i >= j) Console.WriteLine("This won't be executed");
+            if (i > j) Console.WriteLine("This won't be executed");
+            b1 = true;
+            b2 = false;
+            if (b1 & b2) Console.WriteLine("This won't be executed");
+            if (b1 | b2) Console.WriteLine("true");
+            if (!(b1 & b2)) Console.WriteLine("opposite o b1&b2");
+            if (b1 ^ b2) Console.WriteLine("b1 ^ b2 is true");
+            Console.ReadLine();
+        }
+    }
+}
