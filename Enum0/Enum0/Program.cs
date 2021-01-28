c09f318ff175d5ad23af11afdf456070ccfd6352
Pushkar Sudhir Baviskar <b.pushkar745@gmail.com>
2021-01-28 11:19:42 +0530

    Changes in Enum And Interface

---
+++ Enum0/Enum0/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum0
+{
+    class Program
+    {
+        enum Friends{
+            Pushkar,
+           Manasi,
+
+        }
+        static void Main(string[] args)
+        {
+            Friends f = Friends.Manasi;
+            Console.WriteLine(f);
+            Console.ReadLine();
+        }
+    }
+}
