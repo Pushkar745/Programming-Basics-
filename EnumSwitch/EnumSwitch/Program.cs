d3001c819fc5d204e07a90b893d1ef3c6e26ff62
Pushkar Sudhir Baviskar <b.pushkar745@gmail.com>
2021-01-28 11:35:14 +0530

    Comment

--- EnumSwitch/EnumSwitch/Program.cs
+++ EnumSwitch/EnumSwitch/Program.cs
@@ -8,6 +8,7 @@ namespace EnumSwitch
 {
     class Program
     {
+        //Use enums when you have values that you know aren't going to change, like month days, days, colors, deck of cards, etc
         enum Level
         {
             Low,
