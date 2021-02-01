72eaf1ef4dd6a766ccf6f5765719f0349860c6a7
Pushkar Sudhir Baviskar <b.pushkar745@gmail.com>
2021-02-01 16:51:47 +0530

    BitwiseAND Example

---
+++ BitwiseAND/BitwiseAND/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitwiseAND
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ushort num;
+            num = 10;
+            if ((num & 1) == 1)
+                {
+                Console.WriteLine("This won't display.");
+
+            }
+            num = 11;
+            if ((num & 1) == 1)
+                Console.WriteLine(num + "is Odd");
+            Console.ReadKey();
+
+        }
+    }
+}
