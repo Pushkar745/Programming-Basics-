0a8d47ab1993dbb6d698c9a67a1a47cc99bc5345
Pushkar Sudhir Baviskar <b.pushkar745@gmail.com>
2021-02-03 14:23:12 +0530

    Demostrate the NOT Operator

---
+++ NOTOperator/NOTOperator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NOTOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte b = -34;
+            for(int t=128;t>0;t=t/2)
+            {
+                if ((b & t) != 0) Console.Write("1 ");
+                if ((b & t) == 0) Console.Write("0 ");
+            }
+            Console.WriteLine();
+            //reverse all bits
+            b = (sbyte)~b;
+            for(int t=128;t>0;t=t/2)
+            {
+                if ((b & t) != 0) Console.Write("1 ");
+                if ((b & t) == 0) Console.Write("0 ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
