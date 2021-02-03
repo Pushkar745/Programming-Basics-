a250e492f120daafd75bcba70f8ed493991771cc
Pushkar Sudhir Baviskar <b.pushkar745@gmail.com>
2021-02-03 14:44:23 +0530

    Prevent a division by zero using th ?

---
+++ ConditionalOperator/ConditionalOperator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConditionalOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result;
+            for(int i=-5;i<6;i++)
+            {
+                result = i != 0 ? 100 / i : 0;
+                if (i != 0)
+                    Console.WriteLine("100 /" + i + "is" + result);
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
