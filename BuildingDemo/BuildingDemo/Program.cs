f4a96b48a048b64364e855cb78ef6d6ef6ffe81b
Pushkar Sudhir Baviskar <b.pushkar745@gmail.com>
2021-02-06 13:24:36 +0530

    Normal class Methods , and Garbage collection Demo With Destrutor

---
+++ BuildingDemo/BuildingDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingDemo
+{
+    class Program
+    {
+        class Building
+        {
+            public int Floors;//Numbers of floors
+            public int Area;
+            public int Occupants;
+        }
+        static void Main(string[] args)
+        {
+            //New is Operator which dynamically allocates memory for an object 
+            //allocates at RunTime
+            Building house = new Building();//creating house as instance 
+            int areaPP;
+            house.Occupants = 8;
+            house.Area = 2500;
+            house.Floors = 2;
+            areaPP = house.Area / house.Occupants;
+            Console.WriteLine("house has \n" + house.Floors + "floors\n" + house.Occupants + "occupants\n" + house.Area + "total area\n" + areaPP + "area per person");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DestructorDemo/DestructorDemo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DestructorDemo
+{
+    class Program
+    {
+        class Destruct
+        {
+            public int x;
+            public Destruct(int i)
+            {
+                x = i;
+            }
+
+            ~Destruct()
+            {
+                Console.WriteLine("Destructing " + x);
+            }
+            //Generate an object that is immediately destroyed
+            public void Generator(int i)
+            {
+                Destruct o = new Destruct(i);
+            }
+        }
+        //called when object is recycled
+       
+        static void Main(string[] args)
+        {
+            int count;
+            Destruct ob = new Destruct(0);
+            /* Now ,Generate a large number of object .Atsome point,Garbage collection will occur.
+             Note: You might need to increase the numeber of objects generated in order to force garbage collection
+             */
+            for (count = 1; count < 100000; count++)
+                ob.Generator(count);
+            Console.WriteLine("Done!");
+            Console.ReadLine();
+
+        }
+    }
+}
