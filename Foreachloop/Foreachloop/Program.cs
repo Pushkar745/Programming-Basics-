6a4e4d2dd3a2df3b2bd9a1d901cdf17ff038e915
Pushkar Sudhir Baviskar <b.pushkar745@gmail.com>
2021-02-10 17:32:05 +0530

    Foreachexample

---
+++ Foreachloop/Foreachloop/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foreachloop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int[] nums = new int[12];
+            //Give nums some values
+            for (int i = 0; i <= 10; i++)
+                nums[i] = i;
+            //Use foreach to display and sum the values
+            foreach(int x in nums)
+            {
+                Console.WriteLine("Value is :" + x);
+                sum += x;
+
+            }
+            Console.WriteLine("Summation: " + sum);
+            Console.ReadLine();
+        }
+    }
+}
