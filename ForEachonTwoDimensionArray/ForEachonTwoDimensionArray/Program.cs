8d0bef2b7f345387bb1f973fffe200c5b70fadcc
Pushkar Sudhir Baviskar <b.pushkar745@gmail.com>
2021-02-11 12:19:47 +0530

    ForEach using MultiDimensional Array

---
+++ ForEachonTwoDimensionArray/ForEachonTwoDimensionArray/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForEachonTwoDimensionArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int[,] nums = new int[3, 5];
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 5; j++)
+                    nums[i, j] = (i + 1) * (j + 1);
+            foreach (int x in nums)
+            {
+                Console.WriteLine("Value is: " + x);
+                sum += x;
+            }
+            Console.WriteLine("Summation: " + sum);
+            Console.ReadLine();
+        }
+    }
+}
