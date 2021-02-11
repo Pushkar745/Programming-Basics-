589f7902f795d1e9878eb0cd4b5be3fb774d44f4
Pushkar Sudhir Baviskar <b.pushkar745@gmail.com>
2021-02-11 12:25:13 +0530

    Seraching of array using foreach loop

---
+++ ForeachArraySearch/ForeachArraySearch/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForeachArraySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = new int[12];
+            int val;
+            bool found = false;
+            //Give Nums some value
+            for (int i = 0; i < 10; i++)
+                num[i] = i;
+            val = 5;
+            //Use foreach to serach nums for key
+            foreach(int x in num)
+            {
+                if(x==val)
+                {
+                    found = true;
+                    break;
+                }
+            }
+            if (found)
+                Console.WriteLine("Value found");
+            Console.ReadLine();
+        }
+    }
+}
