94a765c8fb485aa9e02de78600eca2f10834f8a1
Pushkar Sudhir Baviskar <b.pushkar745@gmail.com>
2021-02-16 16:27:44 +0530

    SubstringConvertDigitToword Public private class

---
+++ PublicPrivateclass/PublicPrivateclass/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PublicPrivateclass
+{
+    class MyClass
+    {
+        private int alpha;
+        int beta;
+        public int gamma;
+        public void SetAlpha(int a)
+        {
+            alpha = a;
+        }
+        public int GetAlpha()
+        {
+            return alpha;
+        }
+        public void SetBeta(int a)
+        {
+            beta = a;
+
+        }
+        public int GetBeta()
+        {
+            return beta;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass ob = new MyClass();
+            //Access to alpha and beta is allowed only through methods
+            ob.SetAlpha(55);
+            ob.SetBeta(21);
+            Console.WriteLine("ob.alpha is " + ob.GetAlpha());
+            Console.WriteLine("Ob.Beta is " + ob.GetBeta());
+            //You can not access alpha or beta like this 
+            //ob.alpha = 10;\
+            //It is OK to directly access gamma because it is public
+            ob.gamma = 99;
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Substring/Substring/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Pushkar ";
+            string sub = s.Substring(0, 4);
+            Console.WriteLine(s);
+            Console.WriteLine(sub);
+            Console.ReadLine();
+        }
+    }
+}
